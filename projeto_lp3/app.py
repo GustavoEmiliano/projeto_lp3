521b612b04bd6be835af6c254ec5175c9b302e98
Gustavo Emiliano <gustavoemiliano2014@gmail.com>
2024-06-14 21:14:29 -0300

    Exercício da reposição sobre rotas: Retornar cpfs e cnpjs aleatórios

--- projeto_lp3/app.py
+++ projeto_lp3/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from validate_docbr import CPF, CNPJ
+
+cpf = CPF()
+cnpj = CNPJ()
 
 app = Flask("Minha App")
 
 # Uma página do flask é igual a: rota + função
- 
+
 #/ home page - Home 
 @app.route("/")
 
@@ -21,3 +25,41 @@ def contato():
 
 def produto():
     return "<h1> Produtos Legais</h1>"
+
+# Abaixo está o exercicio sobre rotas, praticado em aula de reposição.
+
+# página/servicos retornar "Nossos Serviços"
+# pagina /gerar-cpf retornar Cpf Aleatorio
+# pagina /gerar-cnpj retornar Cnpj aleatório
+
+
+# /servicos - pagina serviços
+
+@app.route("/servicos")
+def servicos():
+    return "<h1> Nossos Serviços </h1>"
+
+
+@app.route("/gerar-cpf")
+def gerar_cpf():
+    return f"CPF: {cpf.generate(True)}"
+
+@app.route("/gerar-cnpj")
+def gerar_cnpj():
+    return f"CNPJ: {cnpj.generate(True)}"
+
+@app.route("/gerar-5-cpfs")
+def gerar_5_cpfs():
+    cpfs = []
+    for i in range(5):
+        cpfs.append(cpf.generate(True))
+    return  f'CPFS: {cpfs} '
+
+@app.route("/gerar-5-cnpjs")
+def gerar_5_cnpjs():
+    cnpjs = []
+    for i in range(5):
+        cnpjs.append(cnpj.generate(True))
+    return  f'CNPJS: {cnpjs} '
+
+app.run(debug=True)
